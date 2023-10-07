a81c56f3b516e500e31589312cf699a06f0a5d98
ReaganWu <wuzhuoyu11@gmail.com>
2023-10-07 15:05:09 +0800

    Add import in Model file

---
+++ Model.py
@@ -0,0 +1,10 @@
+'''
+Author: ReaganWu wuzhuoyu11@gmail.com
+Date: 2023-10-07 14:14:48
+LastEditors: ReaganWu wuzhuoyu11@gmail.com
+LastEditTime: 2023-10-07 15:04:06
+FilePath: /MonteCarlo_Reagan/Model.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+import torch as tor
+import torch.nn as nn
